2282cd23dfc80b96f9818bdb0924dcafb4137a26
Diego Garcia <garcia.diegoandres@gmail.com>
2015-12-15 12:31:01 -0500

    Devolver usuarios sin propietario

--- SubastaVacunos/src/co/edu/udistrtital/subasta/control/gestor/GestorUsuarios.java
+++ SubastaVacunos/src/co/edu/udistrtital/subasta/control/gestor/GestorUsuarios.java
@@ -7,6 +7,7 @@ import java.util.List;
 import co.edu.udistrtital.subasta.control.dto.ComboItemDTO;
 import co.edu.udistrtital.subasta.control.peticiones.IEstrategiaPeticion;
 import co.edu.udistrtital.subasta.dao.UsuarioDAO;
+import co.edu.udistrtital.subasta.modelo.catalogo.Producto;
 import co.edu.udistrtital.subasta.modelo.usuario.Usuario;
 
 public class GestorUsuarios {
@@ -21,6 +22,20 @@ public class GestorUsuarios {
 	
 	public List<ComboItemDTO> getUsuarios(){
 		Iterator<Usuario> usuarios=usuarioDAO.getUsuarios().iterator();
+		List<ComboItemDTO> usuariosAMostrar = new ArrayList<ComboItemDTO>();
+		while (usuarios.hasNext()) {
+			Usuario usuario = (Usuario) usuarios.next();
+			ComboItemDTO item = new ComboItemDTO();
+			item.setKey(String.valueOf(usuario.getCodigo()));
+			item.setValue(usuario.getNombre());
+			usuariosAMostrar.add(item);
+		}
+		return usuariosAMostrar;
+	}
+	
+	public List<ComboItemDTO> getUsuariosSinPropietario(Producto producto){
+		
+		Iterator<Usuario> usuarios=usuarioDAO.getUsuariosMenosUno(producto.getPropietario()).iterator();
 		List<ComboItemDTO> usuariosAMostrar = new ArrayList<ComboItemDTO>();
 		while (usuarios.hasNext()) {
 			Usuario usuario = (Usuario) usuarios.next();
