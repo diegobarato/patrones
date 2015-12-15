d7111733ece8c8cbe792c6cd00e95fb357f3f8a8
diegobarato <diegobarato@seven4n.com>
2015-12-15 13:18:52 -0500

    ajuste crear oferta

--- SubastaVacunos/src/co/edu/udistrtital/subasta/vista/CrearOferta.java
+++ SubastaVacunos/src/co/edu/udistrtital/subasta/vista/CrearOferta.java
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-package co.edu.udistrtital.subasta.vista;
-
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Iterator;
-
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.TitledBorder;
-
 package co.edu.udistrtital.subasta.vista;
 
 import java.awt.BorderLayout;
