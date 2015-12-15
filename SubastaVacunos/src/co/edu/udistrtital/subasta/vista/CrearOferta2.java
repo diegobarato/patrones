ca5f4c84083e205e39905c728eedb2b6d6c8aa44
Diego Garcia <garcia.diegoandres@gmail.com>
2015-12-15 12:56:52 -0500

    cambio de formulario de puja

---
+++ SubastaVacunos/src/co/edu/udistrtital/subasta/vista/CrearOferta2.java
@@ -0,0 +1,92 @@
+package co.edu.udistrtital.subasta.vista;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import co.edu.udistrtital.subasta.control.dto.ComboItemDTO;
+import co.edu.udistrtital.subasta.control.gestor.GestorProductos;
+import co.edu.udistrtital.subasta.control.gestor.GestorSubastas;
+import co.edu.udistrtital.subasta.modelo.subasta.Puja;
+import co.edu.udistrtital.subasta.modelo.subasta.Subasta;
+
+public class CrearOferta2 extends JDialog {
+
+	private GestorSubastas gestorSubastas;
+	private GestorProductos gestorProductos;
+	
+	private final JPanel contentPanel = new JPanel();
+	private JComboBox productoCombo;
+	private JTextField precioOferta;
+	private JButton btnOfertar;
+	
+	public CrearOferta2() {
+		gestorSubastas = new GestorSubastas();
+		gestorProductos = new GestorProductos("VACUNO");
+		setTitle("Crear oferta");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(null, "Datos dela puja", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panel.setBounds(43, 24, 345, 181);
+			contentPanel.add(panel);
+			panel.setLayout(new GridLayout(0, 2, 0, 10));
+			{
+				JLabel lblNewLabel = new JLabel("Producto");
+				panel.add(lblNewLabel);
+			}
+			
+			productoCombo = new JComboBox();
+			Iterator<ComboItemDTO> productosItems= gestorSubastas.getProductosOfertados().iterator();
+			while (productosItems.hasNext()) {
+				ComboItemDTO productoItem = (ComboItemDTO) productosItems.next();
+				productoCombo.addItem(productoItem);
+			}
+			panel.add(productoCombo);
+			
+			JLabel lblNewLabel_1 = new JLabel("Precio oferta");
+			panel.add(lblNewLabel_1);
+			
+			precioOferta = new JTextField();
+			panel.add(precioOferta);
+			precioOferta.setColumns(10);
+			
+			
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			
+			JLabel mensaje = new JLabel("");
+			buttonPane.add(mensaje);
+			
+			btnOfertar = new JButton("Enviar Datos");
+			btnOfertar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					String respuesta = gestorSubastas.ofertar(1, Double.parseDouble(precioOferta.getText()),Integer.parseInt(((ComboItemDTO)productoCombo.getSelectedItem()).getKey()));
+					mensaje.setText(respuesta);
+				}
+			});
+			btnOfertar.setActionCommand("OK");
+			buttonPane.add(btnOfertar);
+			getRootPane().setDefaultButton(btnOfertar);
+		}
+	}
+}
---
+++ SubastaVacunos/src/co/edu/udistrtital/subasta/vista/CrearPuja2.java
@@ -0,0 +1,96 @@
+package co.edu.udistrtital.subasta.vista;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import co.edu.udistrtital.subasta.control.gestor.GestorSubastas;
+
+public class CrearPuja2 extends JDialog {
+	
+	private GestorSubastas gestorSubastas;
+	
+	private final JPanel contentPanel = new JPanel();
+	private JTextField precioInicial;
+	private JTextField horaInicio;
+	private JTextField horaTerminacion;
+	private JButton enviarDatos;
+	
+	public CrearPuja2() {
+		gestorSubastas = new GestorSubastas(1);
+		setTitle("Crear puja");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(null, "Datos dela puja", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panel.setBounds(43, 24, 345, 181);
+			contentPanel.add(panel);
+			panel.setLayout(new GridLayout(0, 2, 0, 10));
+			{
+				JLabel lblNewLabel = new JLabel("Precio Inicial");
+				panel.add(lblNewLabel);
+			}
+			
+			precioInicial = new JTextField();
+			panel.add(precioInicial);
+			precioInicial.setColumns(10);
+			
+			JLabel lblNewLabel_1 = new JLabel("Hora Inicio");
+			panel.add(lblNewLabel_1);
+			
+			horaInicio = new JTextField();
+			panel.add(horaInicio);
+			horaInicio.setColumns(10);
+			
+			JLabel lblNewLabel_2 = new JLabel("Hora Terminacion");
+			panel.add(lblNewLabel_2);
+			
+			horaTerminacion = new JTextField();
+			panel.add(horaTerminacion);
+			horaTerminacion.setColumns(10);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			
+			enviarDatos = new JButton("Enviar Datos");
+			enviarDatos.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					String[] inicio = horaInicio.getText().split(":");
+					String[] fin = horaTerminacion.getText().split(":");
+					Calendar calInicio = Calendar.getInstance();
+					calInicio.set(Calendar.HOUR_OF_DAY, Integer.parseInt(inicio[0]));
+					calInicio.set(Calendar.MINUTE, Integer.parseInt(inicio[1]));
+					Calendar calFin = Calendar.getInstance();
+					calFin.set(Calendar.HOUR_OF_DAY, Integer.parseInt(fin[0]));
+					calFin.set(Calendar.MINUTE, Integer.parseInt(fin[1]));
+					gestorSubastas.crearSubasta( Double.valueOf(precioInicial.getText()), calInicio.getTime(), calFin.getTime());
+					
+				}
+			});
+			enviarDatos.setActionCommand("OK");
+			buttonPane.add(enviarDatos);
+			getRootPane().setDefaultButton(enviarDatos);
+		}
+	}
+
+	public void setGestorSubastas(GestorSubastas gestorSubastas) {
+		this.gestorSubastas = gestorSubastas;
+	}
+}
